eb9d5b1f21d790d52af9310da614c650b5af0201
MehrinFirdousi <mehrinfirdousi@gmail.com>
2023-07-29 10:34:02 +0400

    Tested CGI files, changed paths in index files

--- assets/cgi-examples/decision/cgi-bin/buttons.py
+++ assets/cgi-examples/decision/cgi-bin/buttons.py
@@ -59,8 +59,8 @@ form = cgi.FieldStorage() # instantiate only once!
 btn = form.getfirst('buttonType', 'empty')
 
 # Printing env variables
-# for name, value in os.environ.items():
-#     print("{0}: {1}".format(name, value), file=sys.stderr)
+for name, value in os.environ.items():
+    print("{0}: {1}".format(name, value), file=sys.stderr)
 
 # Avoid script injection escaping the user input
 btn = html.escape(btn)
